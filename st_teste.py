23acaaa25532efb2b01788a09bc4fb79359efb97
Guilherme Bittencourt <guilherme.assis.bittencourt@gmail.com>
2024-06-28 18:36:46 -0300

    alteração do site

--- st_teste.py
+++ st_teste.py
@@ -1,4 +1,13 @@
 import streamlit as st
+import request
+
+cep = st.text_input('Digite o CEP:')
+url = f'https://viacep.com.br/ws/{cep}/json/'
+try:
+  response = request.get(url)
+  dados = response.json()
+except:
+  st.error('CEP incorreto ou inexistente')
 
 st.title('SENAC')
 st.markdown('Feito no Senac')
